97453bce399350f79b470c01cc2bb6006f4d59c7
mgh3326 <mgh3326@naver.com>
2019-06-09 17:15:56 +0900

    18 update

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'read',
+      selected_content_id: 2,
       subject: {
         title: 'WEB', sub: 'World Wide Web!'
-      }
-      ,
+      },
+      welcome: {
+        title: 'Welcome', desc: "Hello, React!!"
+      },
       contents: [
         {id: 1, title: "HTML", desc: "HTML is for information"},
         {id: 2, title: "CSS", desc: "CSS is for design"},
@@ -22,11 +26,37 @@ class App extends Component {
   }
 
   render() {
+    console.log('App render');
+    let _title, _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+    }
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}/>
-        <TOC data={this.state.contents}/>
-        <Content title={"HTML"} desc={"HTML is HyperText Markup Language."}/>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}
+                 onChangePage={function () {
+                   this.setState({mode: 'welcome'});
+                 }.bind(this)}/>
+        <TOC onChangePage={function (id) {
+          this.setState({
+            mode: 'read',
+
+            selected_content_id: Number(id)
+          });
+        }.bind(this)} data={this.state.contents}/>
+        <Content title={_title} desc={_desc}/>
       </div>
     );
   }
